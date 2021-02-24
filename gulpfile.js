8d6965d4d6d8f1539ea0ef98891afee499576fce
makstravm <maks7-2-94@gmail.com>
2021-02-25 01:27:22 +0200

    added fixed top menu

--- gulpfile.js
+++ gulpfile.js
@@ -13,7 +13,7 @@ function browsersync(){
     server:{
       baseDir:'app/'
     },
-    notofy:false
+    notify:false
   })
 }
 
@@ -41,20 +41,20 @@ function scripts(){
   .pipe(browserSync.stream())
 }
 
-function images(){
+function images() {
   return src('app/images/**/*.*')
-  .pipe(imagemin([
-    imagemin.gifsicle({interlaced: true}),
-    imagemin.mozjpeg({quality: 75, progressive: true}),
-    imagemin.optipng({optimizationLevel: 5}),
-    imagemin.svgo({
+    .pipe(imagemin([
+      imagemin.gifsicle({ interlaced: true }),
+      imagemin.mozjpeg({ quality: 75, progressive: true }),
+      imagemin.optipng({ optimizationLevel: 5 }),
+      imagemin.svgo({
         plugins: [
-            {removeViewBox: true},
-            {cleanupIDs: false}
+          { removeViewBox: true },
+          { cleanupIDs: false }
         ]
-    })
-  ]))
-  .pipe(dest('dist/images'))
+      })
+    ]))
+    .pipe(dest('dist/images'))
 }
 
 function build(){
